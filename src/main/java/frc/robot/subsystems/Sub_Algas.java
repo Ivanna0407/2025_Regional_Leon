c6e1b0f10687ea74270d001064105754211aeec0
Ivanna Martinez <A00573772@tec.mx>
2025-02-17 22:04:20 -0600

    falta prueba de pigeon, esqueleto de subsistemas

---
+++ src/main/java/frc/robot/subsystems/Sub_Algas.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Sub_Algas extends SubsystemBase {
+  /** Creates a new Sub_Algas. */
+  public Sub_Algas() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
